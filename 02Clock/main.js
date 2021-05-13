45d136568c1ce886b1148e503806d5014a009f32
Oryam <48219965+or-yam@users.noreply.github.com>
2021-05-13 17:23:40 +0300

    Added animation and refactoring

--- 02Clock/main.js
+++ 02Clock/main.js
@@ -1,14 +1,12 @@
-const minHand = document.getElementById('min');
 const secHand = document.getElementById('sec');
+const minHand = document.getElementById('min');
 const hourHand = document.getElementById('hour');
 
 const setTime = () => {
   const now = new Date();
-  minHand.style.transform = `rotate(${now.getMinutes() * 6}deg)`;
   secHand.style.transform = `rotate(${now.getSeconds() * 6}deg)`;
+  minHand.style.transform = `rotate(${now.getMinutes() * 6}deg)`;
   hourHand.style.transform = `rotate(${now.getHours() * 30}deg)`;
 };
 
-setInterval(() => {
-  setTime();
-}, 1000);
+setInterval(setTime, 1000);
